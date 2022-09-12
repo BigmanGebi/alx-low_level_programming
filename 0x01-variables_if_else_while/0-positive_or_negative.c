24e476cfc1ee233291442f9be3006dfce7e87bdb
Gebi <gebijeremiah@gmail.com>
2022-09-12 07:33:17 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,26 +6,19 @@
  *
  *
  * Return: always return 0
-*/
+ */
 int main(void)
 {
 	int n;
-	int lastn;
 
-	srand(time (0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastn = n % 10;
-
-	if (lastn > 5);
-	{
-		printf("last digit of %d is  greater than 5\n", lastn);
-	} if (lastn == 0);
-	{	
-		printf("last digit of %d is  0\n", lastn);
-	} 
-	{
-		printf("last digit of %d is less than 6 and not 0\n", lastn);
-	}
-return (0);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
 
