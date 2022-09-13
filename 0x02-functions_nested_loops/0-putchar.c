d2c794c47ff218494e18ad62678570c8401685ff
Gebi <gebijeremiah@gmail.com>
2022-09-13 16:34:40 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print _putchar followed by a new line
  *
@@ -16,4 +16,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+i
